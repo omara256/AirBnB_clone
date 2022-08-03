8fe276e01287db761861e3c9273480e7283f9f66
jacobnzala <63255452+jacobnzala@users.noreply.github.com>
2022-08-03 21:11:35 +0200

    Create __init__.py

---
+++ models/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+    __init__ modules
+"""
+
+from .base_model import BaseModel
+from .user import User
+from .review import Review
+from .city import City
+from .amenity import Amenity
+from .place import Place
+from .state import State
+
